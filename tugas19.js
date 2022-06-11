4ef3016166f816bbc47a848ba0e9d52794bd9b73
Vall-Justice <104562832+Vall-Justice@users.noreply.github.com>
2022-06-11 14:13:02 +0700

    Tugas 19 Javascript

---
+++ tugas19.js
@@ -0,0 +1,8 @@
+let semangat = "Semangat...";
+console.log(semangat.repeat(5));
+
+
+let jumlah = [5, 20, 45, 64, 45, 20, 5]
+for(let i = 0; i < jumlah.length; i++) {
+   console.log("*".repeat(jumlah[i]));
+}
